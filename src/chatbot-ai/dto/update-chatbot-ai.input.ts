957aae8e58423a96c6f947870d05ee8d9888038d
YosraElmimouni <yosraelmimouni2003@gmail.com>
2026-08-26 17:15:50 +0200

    mediaq

--- src/chatbot-ai/dto/update-chatbot-ai.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateChatbotAiInput } from './create-chatbot-ai.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateChatbotAiInput extends PartialType(CreateChatbotAiInput) {
-  @Field(() => Int)
-  id!: number;
-}
