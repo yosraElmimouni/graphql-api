704e3671154a5fea36e9e99a9b02ac29169053ca
YosraElmimouni <yosraelmimouni2003@gmail.com>
2026-08-26 17:13:04 +0200

    chatbot

--- src/auth/dto/microsoft-login.input.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, Field } from '@nestjs/graphql';
-import { IsNotEmpty } from 'class-validator';
-
-@InputType()
-export class MicrosoftLoginInput {
-  @Field()
-  @IsNotEmpty({ message: 'idToken requis' })
-  idToken!: string;
-}
