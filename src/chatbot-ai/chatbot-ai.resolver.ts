06cb8c105af7ac43eeef2f7ae3f4db82f7039e85
YosraElmimouni <yosraelmimouni2003@gmail.com>
2026-08-26 17:08:26 +0200

    chatbot

---
+++ src/chatbot-ai/chatbot-ai.resolver.ts
@@ -0,0 +1,38 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { ChatbotAiService } from './chatbot-ai.service';
+import { ChatbotAi } from './entities/chatbot-ai.entity';
+import { CreateChatbotAiInput } from './dto/create-chatbot-ai.input';
+import { UpdateChatbotAiInput } from './dto/update-chatbot-ai.input';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
+
+@Resolver(() => ChatbotAi)
+@UseGuards(GqlAuthGuard)
+export class ChatbotAiResolver {
+  constructor(private readonly chatbotAiService: ChatbotAiService) {}
+
+  @Mutation(() => ChatbotAi)
+  createChatbotAi(@Args('createChatbotAiInput') createChatbotAiInput: CreateChatbotAiInput) {
+    return this.chatbotAiService.create(createChatbotAiInput);
+  }
+
+  @Query(() => [ChatbotAi], { name: 'chatbotAis' })
+  findAll() {
+    return this.chatbotAiService.findAll();
+  }
+
+  @Query(() => ChatbotAi, { name: 'chatbotAi' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.chatbotAiService.findOne(id);
+  }
+
+  @Mutation(() => ChatbotAi)
+  updateChatbotAi(@Args('updateChatbotAiInput') updateChatbotAiInput: UpdateChatbotAiInput) {
+    return this.chatbotAiService.update(updateChatbotAiInput.id, updateChatbotAiInput);
+  }
+
+  @Mutation(() => ChatbotAi)
+  removeChatbotAi(@Args('id', { type: () => Int }) id: number) {
+    return this.chatbotAiService.remove(id);
+  }
+}
